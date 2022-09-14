fac8af1dfb8b0cebb75460b824631288f002de8f
Arely Rivera Gonzalez <arelyrivera1996@gmail.com>
2022-09-13 19:35:24 -0700

    added employee, manager, engineer, and intern classes with placeholder methods

---
+++ index.js
@@ -0,0 +1,14 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateHTML = require("./src/generateHTML")
+
+
+// use inquirer to get user input
+
+
+// function to create html file for team (think fs write file )
+    
+
+// at start- init() function
+// 
+
---
+++ src/generateHTML.js
@@ -0,0 +1,13 @@
+
+// make email clickable in html 
+    // default email program opens and populates the TO field of the email with the address
+
+// make github username clickable
+    // that GitHub profile opens in a new tab
+
+function generateHTML(answers){
+    console.log("IT WORKS WOOOO")
+    return `html`
+}
+
+module.exports = generateHTML;

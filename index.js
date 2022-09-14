0164bd776d3dc59dfa08d93b8c18e6d4bc9fbd40
Arely Rivera Gonzalez <arelyrivera1996@gmail.com>
2022-09-13 19:58:29 -0700

    added function writeToFile, and init

--- index.js
+++ index.js
@@ -2,13 +2,36 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const generateHTML = require("./src/generateHTML")
 
+// todo an array of questions for user input (maybe this will change depending on classes..)
+const questions = [
+    {
+        type: "input", 
+        name: "name", 
+        message: "what is your name?"
+    },
+    {
+        type: "list", 
+        name: "favFood", 
+        message: "what is your fav food?",
+        choices: ["tacos", "nachos", "no food"]
+    }
+]
 
-// use inquirer to get user input
+// function to create html file for team
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err)=>
+        (err) ? console.log("error") : console.log("HTML File Generated")
+    )
+}
 
+// function to use inquirer to get user input at start
+function init() {
+    inquirer.prompt (questions)
+    .then((answers)=>{
+        const htmlOutput = generateHTML(answers)
+        writeToFile("team.html", htmlOutput)
+    })
+}
 
-// function to create html file for team (think fs write file )
-    
-
-// at start- init() function
-// 
-
+// Function call to initialize app
+init();
--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,13 +1,13 @@
 
-// make email clickable in html 
-    // default email program opens and populates the TO field of the email with the address
+// todo make email clickable in html 
+    // ? default email program opens and populates the TO field of the email with the address
 
-// make github username clickable
-    // that GitHub profile opens in a new tab
+// todo make github username clickable
+    // ? that GitHub profile opens in a new tab
 
 function generateHTML(answers){
     console.log("IT WORKS WOOOO")
-    return `html`
+    return `html ${answers.name} and ${answers.favFood}`
 }
 
 module.exports = generateHTML;

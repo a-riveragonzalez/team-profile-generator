f173b755f308444177cec5674a6d90333f783a6a
Arely Rivera Gonzalez <arelyrivera1996@gmail.com>
2022-09-13 21:26:50 -0700

    added renderEmployeeCards function

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,9 +1,21 @@
+const { default: ListPrompt } = require("inquirer/lib/prompts/list") //? what is this?
 
-// loop?
+// todo function that returns the license link
+function renderEmployeeCards(typeEmployee) {
+    if (typeEmployee==="Engineer"){
+      return `I'm an  engineer`
+    }
+    if (typeEmployee==="Intern"){
+      return `I'm an Intern`
+    }
+    if (typeEmployee==="No More Employees"){
+      return ``
+    }  
+}
 
 // todo make html 
 function generateHTML(answers){
-    return `html ${answers.name} and ${answers.favFood}`
+    return `html ${answers.name} and ${renderEmployeeCards(answers.typeEmployee)}`
 
     // todo make email clickable in html 
     // ? default email program opens and populates the TO field of the email with the address
